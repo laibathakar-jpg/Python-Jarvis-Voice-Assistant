4dcb2301beb8eb9b6ca236a32be91385282bc6ca
laibathakar-jpg <laibathakar@gmail.com>
2026-06-24 13:19:52 +0500

    Jarvis Ai Assistant

---
+++ musiclibrary.py
@@ -0,0 +1,4 @@
+music={
+    'laal pari':"https://youtu.be/KGn-erOG-Bs?list=RDKGn-erOG-Bs",
+    'tu haqeeqat':'https://www.youtube.com/watch?v=3lws1R5ouPw&list=RD3lws1R5ouPw&start_radio=1&pp=ygUEc29uZ6AHAQ%3D%3D'
+}
